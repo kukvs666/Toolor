c7767a93aaf9ae0f9252d113a9063e91fa3c9a1c
quentin GAUBERT <quentin.gaubert@wanadoo.fr>
2019-09-02 14:25:50 +0200

    création du plugin Toolor

---
+++ toolor.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Toolor
+ * Plugin URI: https://github.com/kukvs666/Toolor
+ * Description: Ceci est un plugin qui permet, depuis l'interface admin de WP de modifier la couleur du thème
+ * Version: 1.0
+ * Author Name: GAUBERT Quentin
+ * Author URI: https://github.com/kukvs666
+ */
+
+
